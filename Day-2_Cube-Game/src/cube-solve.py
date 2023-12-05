109e0bfd0bd48972adcad7fe559bf21d4f4707b6
Jared <jfr7294@gmail.com>
2023-12-05 16:05:12 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Got day 2 cube, challenge 1 completed.
    	modified:   .gitignore
    	modified:   Day-1_Calibration/Day-1_Prompt.md
    	modified:   Day-2_Cube-Game/src/cube-solve.py

--- Day-2_Cube-Game/src/cube-solve.py
+++ Day-2_Cube-Game/src/cube-solve.py
@@ -1,43 +1,48 @@
+import re
+
 # Total number of cubes in play
 maxRedCount = 12
 maxGreenCount = 13
 maxBlueCount = 14
-redSum = 0
-greenSum = 0
-blueSum = 0
-
-
-
-# 
 gameIndex = 1
 validGameSum = 0
 
-cubeCounts = 0
+def handCheck(hands, indexCount):
+    # Break down the hands into the distinct key/value pairs for red / green / blue
+    for colorPair in hands.split(','):
+        colorPair = colorPair.strip()
+        pattern = re.compile(r'\w*[^\s]')
+        colorPair = pattern.findall(colorPair)
+        cubeHands[colorPair[1]] = int(colorPair[0])
+        
+    if cubeHands["blue"]<=maxBlueCount and cubeHands["red"]<=maxRedCount and cubeHands["green"]<=maxGreenCount:
+        return True
+    else:
+        return False
 
 # Open the file and read in the puzzle data
 with open('puzzle-input.txt') as file:
     # Read in the content for the puzzle game results
-    puzzleResults = file.read().splitlines()
-    
+    puzzleResults = file.read().splitlines()      
     
 for line in puzzleResults:
+    # print(f"Game {gameIndex}")
+          
+    # Split all the hands out coming from each game
     cubeCounts = line.split(":")[1].split(";")
-    print(f'Game {gameIndex} : {cubeCounts}')
-    handIndex = 1
+    # Set up the dictionary to perform the tracking of the color sums for each game
     cubeHands = {"red" : 0, "blue" : 0, "green" : 0}
-    for game in cubeCounts:
-        print(f"Hand {handIndex} : {game}")
-        for
-        
+    handIndex=1
+    gameValidation = True
+    # After breaking down the line for each game, go through the hands in each game
+    for hands in cubeCounts:
+        gameValidation = handCheck(hands,handIndex) & gameValidation
         handIndex += 1
         
+    print(f"Game {gameIndex} Validation: {gameValidation}")
+    if gameValidation:
+        validGameSum += gameIndex
         
-        
-        
-    
-
-    
     gameIndex += 1
 
-
-    
+print(f"Total Game Sum : {validGameSum}")    
